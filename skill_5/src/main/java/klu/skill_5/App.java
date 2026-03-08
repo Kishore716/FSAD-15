09be6db023b4cdf6e21d7ceb1054931d8641e235
Kishore716 <kishore191139@gmail.com>
2026-03-08 14:34:43 +0530

    Add files via upload

---
+++ skill_5/src/main/java/klu/skill_5/App.java
@@ -0,0 +1,17 @@
+package klu.skill_5;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Appconfig.class);
+
+        Student student = context.getBean(Student.class);
+
+        student.display();
+    }
+}
